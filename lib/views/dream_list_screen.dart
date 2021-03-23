316b53bbf7a587882ae362579e57ced5ffe5bfe0
LeQuadratico <45048954+LeQuadratico@users.noreply.github.com>
2021-03-23 21:50:44 +0100

    Date can be changed now

--- lib/views/dream_list_screen.dart
+++ lib/views/dream_list_screen.dart
@@ -95,7 +95,7 @@ class _DreamListScreenState extends State<DreamListScreen> {
         title: Text(dream.title),
         subtitle: Padding(
           padding: EdgeInsets.only(top: 10),
-          child: Text(DateFormat.yMMMMd().format(dream.date) + "\n" + dream.content),
+          child: Text(DateFormat.yMMMMd().format(dream.date) + "\n\n" + dream.content),
         ),
         onTap: () {
           Navigator.pushNamed(context, "/dreamDetails",
